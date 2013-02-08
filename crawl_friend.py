9bbac87c78bed5cc4c59017351130e8f3299834a
r99944049 <r99944049@linux1.csie.ntu.edu.tw>
2013-02-09 00:30:20 +0800

    replace crawl_friend.py output dir

--- crawl_friend.py
+++ crawl_friend.py
@@ -1,5 +1,11 @@
 import friend_crawler.friend_crawler as crawler 
 import sys
 script, filename = sys.argv 
-output_dir = "result/friends/"
+
+output_dir = "/tmp2/r99944049/"
+
+if not os.path.exists( output_dir ) :
+	os.makedirs( output_dir ) 
+
+
 crawler.main( filename , output_dir ) 
--- crawl_userdata.py
+++ crawl_userdata.py
@@ -1,7 +1,11 @@
 import friend_crawler.userdata_crawler as userdata_crawler 
 import sys 
 import os.path , os 
-script , filename, output_dir = sys.argv
+script , filename = sys.argv
+
+output_dir = "/tmp2/r99944049/"
+
 if not os.path.exists( output_dir ) :
 	os.makedirs( output_dir ) 
+
 userdata_crawler.main( filename, output_dir ) 
