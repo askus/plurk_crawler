3c9e1c9bed3034a1e228250e8f55c0e27f58cc96
r99944049 <r99944049@linux10.csie.ntu.edu.tw>
2013-02-12 20:00:07 +0800

    fan_crawler

---
+++ friend_crawler/fan_crawler.py
@@ -0,0 +1,55 @@
+from crawler import plurk_authorize, get_api_result 
+CACHE_LENGTH =10
+
+def get_fan( plurk, user_id , size  ):
+	ret = list()
+	
+	for i in xrange(   (size/CACHE_LENGTH) +1 ):
+		offset = i* CACHE_LENGTH  
+		# cal expect number 
+		if offset + CACHE_LENGTH  <= size  :
+			expect = CACHE_LENGTH 
+		else:
+			expect = size - offset
+		temp = get_api_result( plurk, "/APP/FriendsFans/getFansByOffset", {"user_id":user_id , "limit": expect,  "offset": offset} )
+		if temp == None:
+			break
+		ret.extend( temp ) 
+	if len( ret ) == 0:
+		return None
+	else: 
+		return ret 
+def main( filename, output_dir  ):
+
+	infile = open(filename, "r")
+	filename_prefix= filename.split("/")[-1]
+
+	total_length = len( infile.read().split("\n") )
+	infile.close()
+	infile = open(filename, "r")
+
+	outf = open("%s/%s.fan.txt" %(  output_dir , filename_prefix ), "w") 
+
+	plurk = plurk_authorize( ) 
+
+	i= 0
+	for l in infile:
+		# time counting  
+		print "[%s][%.2f] %d/%d" %( filename_prefix  , ( float(i)/ float(total_length) ) , i, total_length ) 
+		i +=1 
+		# get friends  
+		tmp = l.strip().split("\t")
+		user_id = long(tmp[1] ) # need repair
+		size = int( tmp[0] ) # need repair
+		friends = g_fannd( plurk, user_id,size  ) 
+
+		if friends ==None:
+			continue 
+		for friend_dict in friends:
+			friend_id = friend_dict['id'] 
+			print >> outf, "%d\t%d" %( user_id , friend_id )
+			print >> outf, "%d\t%d" %( friend_id, user_id ) 
+		print "%d: %d/ %d" %( user_id, len( friends ), size)
+	infile.close()
+	outf.close()
+
